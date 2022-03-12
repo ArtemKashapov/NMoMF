69a66d0ab0b8c5ada8fe87dc9f1d2cea8de22970
Anton <voevodin_anton_2000@mail.ru>
2022-03-12 19:54:40 +0400

    refactoring

--- solver.py
+++ solver.py
@@ -6,11 +6,7 @@ from math import pi
 
 
 class State:
-<<<<<<< HEAD
-    def __init__(self, c=1.65, k=0.59, R=5, uc=0, l=0.5, alpha=0.003, T=40, I=50, K=50*8) -> None:
-=======
-    def __init__(self, c=1.65, k=0.59, R=5, uc=0, l=0.5, alpha=0.003, T=40, I=1024, K=1024) -> None:
->>>>>>> 0ac520df3138efb62ba0784a4240fc7f467335e0
+    def __init__(self, c=1.65, k=0.59, R=5, uc=0, l=0.5, alpha=0.003, T=40, I=64, K=64*64) -> None:
         self.c = c
         self.k = k
         self.R = R
@@ -21,6 +17,12 @@ class State:
         self.I = I
         self.K = K
 
+        self.t = np.linspace(0, T, K + 1)
+        self.theta = np.linspace(0.001, pi-0.001, I + 1)
+
+        self.h_t = self.t[2] - self.t[1]
+        self.h_theta = self.theta[2] - self.theta[1]
+
     def psi(self, theta):
         return self.uc + np.cos(theta) ** 4
 
@@ -28,22 +30,17 @@ class State:
 class Implicits:
     def __init__(self, state) -> None:
         self.state = state
-        self.t = np.linspace(0, self.state.T, self.state.K + 1)
-        self.theta = np.linspace(0.001, pi-0.001, self.state.I + 1)
-
-        self.h_t = self.t[2] - self.t[1]
-        self.h_theta = self.theta[2] - self.theta[1]
 
         self.A = self.state.k / (self.state.c * self.state.R ** 2)
         self.B = self.state.alpha / (self.state.c * self.state.l)
 
-        self.gam = - self.A * self.h_t / (self.h_theta ** 2) * (1 + self.h_theta / (2 * np.tan(self.theta[:-1])))
-        self.eps = - self.A * self.h_t / (self.h_theta ** 2) * (1 - self.h_theta / (2 * np.tan(self.theta[1:])))
-        self.eta = self.B * self.h_t * self.state.uc
+        self.gam = - self.A * self.state.h_t / (self.state.h_theta ** 2) * (1 + self.state.h_theta / (2 * np.tan(self.state.theta[:-1])))
+        self.eps = - self.A * self.state.h_t / (self.state.h_theta ** 2) * (1 - self.state.h_theta / (2 * np.tan(self.state.theta[1:])))
+        self.eta = self.B * self.state.h_t * self.state.uc
 
-        sigma = - 2 * self.A * self.h_t / (self.h_theta ** 2)
+        sigma = - 2 * self.A * self.state.h_t / (self.state.h_theta ** 2)
 
-        self.beta = (self.B * self.h_t + 2 * self.A * self.h_t / (self.h_theta ** 2) + 1) * np.ones(self.state.I + 1)
+        self.beta = (self.B * self.state.h_t + 2 * self.A * self.state.h_t / (self.state.h_theta ** 2) + 1) * np.ones(self.state.I + 1)
         self.eps[-1] = sigma
         self.gam[0] = sigma
 
@@ -71,13 +68,13 @@ class Implicits:
         # matrix = np.diag(self.beta) + np.diag(self.eps, -1) + np.diag(self.gam, 1) # для м. м.
 
         v = np.zeros([self.state.K + 1, self.state.I + 1])
-        v[0, :] = self.state.psi(self.theta)
+        v[0, :] = self.state.psi(self.state.theta)
 
         for k_ind in range(1, self.state.K+1):
             eel.setProgress(round((1 - (self.state.K - k_ind + 1) / self.state.K) * 100, 0))
             # v[k_ind, :] = np.linalg.inv(matrix) @ v[k_ind - 1, :] # матричный метод (медленный в 40 раз менее производительный)
             v[k_ind, :] = self.tri_diag_solver_right(self.eps, self.beta, self.gam, v[k_ind - 1, :])
-        return v, self.theta, self.t
+        return v, self.state.theta, self.state.t
 
 
 class GFTool:
@@ -91,12 +88,12 @@ class GFTool:
     def plot_solution(self):
         f = plt.figure(figsize=[12, 6])
         plt.plot(self.theta_array, self.v[0, :], lw=4, c='black', label='t=0')
-        for ind in [int(self.K / 4), int(self.K / 2), int(self.K / 4 * 3), int(self.K - 1)]:
-            plt.plot(self.theta_array, self.v[ind, :], lw=4, label='t='+str(self.time_array[ind]))
+        for coef in [1/4, 1/2, 3/4, 1]:
+            plt.plot(self.theta_array, self.v[int((self.K - 1) * coef), :], lw=4, label='t='+str(self.time_array[int((self.K - 1) * coef)]))
 
         plt.xlim([self.theta_array[1], self.theta_array[-1]])
         plt.xlabel('${\\theta}$', fontsize=20)
-        plt.ylabel('${\\psi(\\theta)}$', fontsize=20)
+        plt.ylabel('${v(\\theta)}$', fontsize=20)
         plt.xticks([0, pi/4, pi/2, 3*pi/4, pi], ['0', '$\\pi / 4$', '$\\pi / 2$', '$3 \\pi / 4$', '$\\pi$'], fontsize=20)
         # plt.yticks([0, 0.5, 1], ['0', '0.5', '1'], fontsize=20)
         plt.legend()
@@ -113,31 +110,19 @@ class Explicits:
     def __init__(self, state) -> None:
         self.state = state
 
-        self.t = np.linspace(0, self.state.T, self.state.K + 1)
-        self.theta = np.linspace(0.001, pi-0.001, self.state.I + 1)
-
-        self.h_t = self.t[2] - self.t[1]
-        self.h_theta = self.theta[2] - self.theta[1]
-
-        self.gam = self.state.k*self.h_t / (self.state.R ** 2 * self.state.c * self.h_theta ** 2)
-        self.beta = self.state.alpha * self.h_t / (self.state.l * self.state.c)
-
-    def omega(self, i):
-        return self.state.k * self.h_t / (2 * self.state.R ** 2 * np.tan(self.theta[i]) * self.h_theta * self.state.c)
-
+        self.gam = state.k * state.h_t / (state.R ** 2) / state.c / (state.h_theta ** 2)
+        self.beta = state.alpha * state.h_t / state.l / state.c
+        self.omega = state.k * state.h_t / (state.R ** 2) / np.tan(state.theta) / 2 / state.h_theta / state.c
 
     def solve(self):
         v = np.zeros([self.state.K + 1, self.state.I + 1])
-        v[0, :] = self.state.psi(self.theta)
+        v[0, :] = self.state.psi(self.state.theta)
 
-        for k_ind in range(1, self.state.K+1):
+        for k_ind in range(1, self.state.K + 1):
             eel.setProgress(round((1 - (self.state.K - k_ind + 1) / self.state.K) * 100, 0))
-            for i_ind in range(0, self.state.I+1):
-                if i_ind == 0:
-                    v[k_ind, i_ind] = (1 - self.beta - 4*self.gam) * v[k_ind - 1, i_ind] + 2*self.gam * v[k_ind - 1, i_ind + 1]
-                elif i_ind == self.state.I:
-                    v[k_ind, i_ind] = (1 - self.beta - 4*self.gam) * v[k_ind - 1, i_ind] + 2*self.gam * v[k_ind - 1, i_ind - 1]
-                else:
-                    v[k_ind, i_ind] = (1 - self.beta - 2* self.gam) * v[k_ind - 1, i_ind] + (self.omega(i_ind) + self.gam) * v[k_ind - 1, i_ind + 1] + (self.gam - self.omega(i_ind)) * v[k_ind - 1, i_ind - 1]
+            v[k_ind, 0] = (1 - self.beta - 2 * self.gam) * v[k_ind - 1, 0] + 2 * self.gam * (v[k_ind - 1, 1])
+            for i_ind in range(1, self.state.I):
+                v[k_ind, i_ind] = (1 - 2 * self.gam - self.beta) * v[k_ind - 1, i_ind] + (self.omega[i_ind] + self.gam) * v[k_ind - 1, i_ind + 1] + (self.gam - self.omega[i_ind]) * v[k_ind - 1, i_ind - 1]
+            v[k_ind, self.state.I] = (1 - self.beta - 2 * self.gam) * v[k_ind - 1, self.state.I] + 2 * self.gam * (v[k_ind - 1, self.state.I - 1])
 
-        return v, self.theta, self.t
+        return v, self.state.theta, self.state.t
